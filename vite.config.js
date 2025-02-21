78de4852120fe77d0fee71305e48057b33bcfd1c
Manzi Fabrice Niyigaba <manzi.fabrice.niyigaba.27@dartmouth.edu.com>
2025-02-21 16:34:11 -0500

    removed favicon

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 import autoprefixer from 'autoprefixer';
-import faviconPlugin from "vite-plugin-favicon";
 
 
 
 export default defineConfig({
-  plugins: [eslint(), faviconPlugin('public/favicon.png')],
+  plugins: [eslint()],
   css: {
     postcss: {
       plugins: [
